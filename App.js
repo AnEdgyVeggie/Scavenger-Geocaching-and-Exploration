fe4311e97a17ccf05fef45f90b9d7329c49178dc
Amanda <amandagurns@hotmail.ca>
2024-12-02 13:02:15 -0500

    User login implemented and functional.

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import NavigationBar from './src/components/NavigationBar';
+import AccountStack  from './src/components/AccountStack';
+
+import { useState } from 'react'
 // import * as DevClient from 'expo-dev-client';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationBar />
-    </View>
-  );
+  export default function App() {
+
+    const [ loggedIn, setLoggedIn ] = useState(false)
+
+
+
+    if (loggedIn) {
+      return (
+        <View style={styles.container}>
+          <NavigationBar />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container} >
+          <AccountStack setLoggedIn={setLoggedIn} />
+        </View>
+      )
+    }
+
+
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
